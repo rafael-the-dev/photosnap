9da28d97e585465045f01ec56fea6c5c45a26dc6
rafael-tivane <luislangabusiness@gmail.com>
2021-07-23 08:47:19 +0100

    Adding new feature to the app, a stories page

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,15 +1,15 @@
 import './styles.scss';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
+import Stories from '../Stories';
 
 function App() {
     return (
         <>
             <Router>
                 <Switch>
-                    <Route>
-                        <Home />
-                    </Route>
+                    <Route exact path="/stories" component={Stories} />
+                    <Route exact path="/" component={Home} /> 
                 </Switch>
             </Router>
         </>
---
+++ src/pages/Stories/index.jsx
@@ -0,0 +1,62 @@
+import Card from '../../components/Card';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import { Link } from 'react-router-dom';
+import './styles.scss';
+
+const lazyLoad = () => {
+   setTimeout(() => {}, 2000);
+    return (
+        <>
+            <Card image="rage-of-the-sea.jpg" name="The mountains" author="by John Appleseed" cardLink="/" />
+            <Card image="running-free.jpg" name="Sunset Cityscapes" author="by Benjamin Cruz" cardLink="/" />
+            <Card image="behind-the-waves.jpg" name="18 Days Voyage" author="by Alexei Borodin" cardLink="/" />
+            <Card image="calm-waters.jpg" name="Architecturals" author="by Samantha Brooke" cardLink="/" />
+            <Card image="milky-way.jpg" name="The mountains" author="by John Appleseed" cardLink="/" />
+            <Card image="dark-forest.jpg" name="Sunset Cityscapes" author="by Benjamin Cruz" cardLink="/" />
+            <Card image="somwarpet.jpg" name="18 Days Voyage" author="by Alexei Borodin" cardLink="/" />
+            <Card image="land-of-dreams.jpg" name="Architecturals" author="by Samantha Brooke" cardLink="/" />
+        </>
+    );
+};
+
+const Stories = () => {
+    return (
+        <>
+            <Header />
+            <main className="px-xl">
+                <section className="highlight">
+                    <div className="highlight__illustration"></div>
+                    <div className="highlight__content">
+                        <h1 className="highlight__title">
+                            Hazy full<br />moon of<br />appalachia
+                        </h1>
+                        <p className="highlight__description">
+                            The dissected plateau area, while not actually made up of 
+                            geological mountains, is popularly called "mountains," especially 
+                            in eastern Kentucky and West Virginia, and while the ridges are 
+                            not high, the terrain is extremely rugged.
+                        </p>
+                        <Link to="/" className="highlight__link">
+                            Read the story <i className="highlight__link--icon "></i>
+                        </Link>
+                    </div>
+                </section>
+                <section class="cards-section">
+                    <Card image="mountains.jpg" name="The mountains" author="by John Appleseed" cardLink="/" />
+                    <Card image="cityscapes.jpg" name="Sunset Cityscapes" author="by Benjamin Cruz" cardLink="/" />
+                    <Card image="18-days-voyage.jpg" name="18 Days Voyage" author="by Alexei Borodin" cardLink="/" />
+                    <Card image="architecturals.jpg" name="Architecturals" author="by Samantha Brooke" cardLink="/" />
+                    <Card image="world-tour.jpg" name="The mountains" author="by John Appleseed" cardLink="/" />
+                    <Card image="unforeseen-corners.jpg" name="Sunset Cityscapes" author="by Benjamin Cruz" cardLink="/" />
+                    <Card image="king-on-africa.jpg" name="18 Days Voyage" author="by Alexei Borodin" cardLink="/" />
+                    <Card image="trip-to-nowhere.jpg" name="Architecturals" author="by Samantha Brooke" cardLink="/" />
+                   
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default Stories;
