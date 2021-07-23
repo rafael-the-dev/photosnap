3d0705b5f6ab1369d63ce42c75f1f26072c6228e
rafael-tivane <luislangabusiness@gmail.com>
2021-07-23 15:35:44 +0100

    Adding responsive princing plans: basic, pro and business plans

--- src/components/Plan/index.jsx
+++ src/components/Plan/index.jsx
@@ -4,11 +4,12 @@ import './styles.scss';
 const Plan = ({ plan, customClass }) => {
     return (
         <article className={`plan ${customClass ? 'plan--pro' : ''}`}>
-            <div className="plan__content">
+            <div className={`plan__content ${customClass ? 'plan__content--pro' : ''}`}>
                 <h2 className="plan__name">{ plan.name }</h2>
                 <p className="plan__description">{ plan.description }</p>
-                <h3 className="plan__price">{ plan.price }</h3>
-                <Link to={ plan.url } className={`plan__link ${customClass ? 'plan__link--pro' : ''}`}>
+                <h3 className="plan__price plan__price--tablet">$ { plan.price }</h3>
+                <Link to={ plan.url } className={`plan__link  
+                    ${customClass ? 'plan__link--pro' : ''}`}>
                     pick plan
                 </Link>
             </div>
--- src/pages/Pricing/index.jsx
+++ src/pages/Pricing/index.jsx
@@ -2,8 +2,73 @@ import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Banner from '../../components/Banner';
 import './styles.scss';
+import Plan from '../../components/Plan';
+import { useEffect, useMemo, useState } from 'react';
+
+const prices = {
+    monthly: {
+        basic: {
+            name: 'Basic',
+            description: 'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+            price: "19.00",
+            url: '/'
+        },
+        pro: {
+            name: 'Pro',
+            description: 'More advanced features available. Recommended for photography veterans and professionals.',
+            price: "39.00",
+            url: '/'
+        },
+        business: {
+            name: 'Business',
+            description: 'Additional features available such as more detailed metrics. Recommended for business owners.',
+            price: "99.00",
+            url: '/'
+        }
+    },
+    yearly: {
+        basic: {
+            name: 'Basic',
+            description: 'Includes basic usage of our platform. Recommended for new and aspiring photographers.',
+            price: "190.00",
+            url: '/'
+        },
+        pro: {
+            name: 'Basic',
+            description: 'More advanced features available. Recommended for photography veterans and professionals.',
+            price: "390.00",
+            url: '/'
+        },
+        business: {
+            name: 'Business',
+            description: 'Additional features available such as more detailed metrics. Recommended for business owners.',
+            price: "990.00",
+            url: '/'
+        }
+    }
+};
+
+const plans = duration => {
+    return (
+        <div>
+            <Plan plan={prices[duration].basic} />
+            <Plan plan={prices[duration].pro} customClass="plan--pro" />
+            <Plan plan={prices[duration].business} />
+        </div>
+    );
+};
 
 const Pricing = () => {
+    const [ duration, setDuration ] = useState('monthly');
+
+    /*const plansMemo = useMemo(() => {
+        return plans(duration);
+    }, [ duration ]); */
+
+    useEffect(() => {
+        setDuration(d => 'monthly');
+    }, []);
+
     return (
         <>
             <Header />
@@ -17,6 +82,13 @@ const Pricing = () => {
                             visual storytellers. It’s the simple way to create and share 
                             your photos.
                         </p>
+                    </div>
+                </section>
+                <section className="plans-section">
+                    <div className="plans-section__division">
+                        <Plan plan={prices[duration].basic} />
+                        <Plan plan={prices[duration].pro} customClass="plan--pro"/>
+                        <Plan plan={prices[duration].business} />
                     </div>
                 </section>
                 <Banner />
