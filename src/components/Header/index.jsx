6c2f538406d087a7becfdd1dcf21d30fd8b97e50
rafael-tivane <luislangabusiness@gmail.com>
2021-07-24 09:33:42 +0100

    Adding drop-down menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,36 @@
 import Logo from '../Logo';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 
 const Header = () => {
+    const navigationRef = useRef(null);
+    const firstLineRef = useRef(null);
+    const secondLineRef = useRef(null);
+
+    useEffect(() => {
+        window.addEventListener('resize', event => {
+            if(event.target.innerWidth > 565) {
+                navigationRef.current.classList.remove('header__navigation--mobile');
+                firstLineRef.current.classList.remove('header__line--first');
+                secondLineRef.current.classList.remove('header__line--second');
+            }
+        });
+
+        return () => window.onresize = null;
+    }, []);
+
+    const clickHandler = () => {
+        navigationRef.current.classList.toggle('header__navigation--mobile');
+        firstLineRef.current.classList.toggle('header__line--first');
+        secondLineRef.current.classList.toggle('header__line--second');
+    };
+
     return (
         <>
             <header className="px-md px-lg header">
                 <Logo />
-                <nav className="header__navigation">
+                <nav ref={navigationRef} className="header__navigation">
                     <ul className="header__list">
                         <li className="header__item">
                             <Link to="/stories" className="link header__link">Stories</Link>
@@ -19,10 +42,13 @@ const Header = () => {
                             <Link to="/pricing" className="link header__link">Pricing</Link>
                         </li>
                     </ul>
+                    <Link to="/" className="header__invite-button header__invite-button--mobile">
+                        Get an invite
+                    </Link>
                 </nav>
-                <button className="header__menu-button">
-                    <span className="header__line"></span>
-                    <span className="header__line"></span>
+                <button onClick={clickHandler} className="header__menu-button">
+                    <span ref={firstLineRef} className="header__line"></span>
+                    <span ref={secondLineRef} className="header__line"></span>
                 </button>
                 <Link to="/" className="header__invite-button">Get an invite</Link>
             </header>
